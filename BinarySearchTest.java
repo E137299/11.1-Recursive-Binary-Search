eea04825712348bfd2c76b20442e2207051485eb
David Yin <29613693+E137299@users.noreply.github.com>
2025-03-06 08:30:29 -0600

    Update BinarySearchTest.java

--- BinarySearchTest.java
+++ BinarySearchTest.java
@@ -7,10 +7,10 @@ public class BinarySearchTest {
     public void testBinarySearch() {
         int[] sortedArray = {1, 3, 5, 7, 9, 11, 13};
 
-        assertEquals(3, BinarySearch.binarySearch(sortedArray, 7, 0, sortedArray.length - 1));
-        assertEquals(0, BinarySearch.binarySearch(sortedArray, 1, 0, sortedArray.length - 1));
-        assertEquals(6, BinarySearch.binarySearch(sortedArray, 13, 0, sortedArray.length - 1));
-        assertEquals(-1, BinarySearch.binarySearch(sortedArray, 6, 0, sortedArray.length - 1));
-        assertEquals(-1, BinarySearch.binarySearch(sortedArray, 14, 0, sortedArray.length - 1));
+        assertEquals(3, Main.binarySearch(sortedArray, 7, 0, sortedArray.length - 1));
+        assertEquals(0, Main.binarySearch(sortedArray, 1, 0, sortedArray.length - 1));
+        assertEquals(6, Main.binarySearch(sortedArray, 13, 0, sortedArray.length - 1));
+        assertEquals(-1, Main.binarySearch(sortedArray, 6, 0, sortedArray.length - 1));
+        assertEquals(-1, Main.binarySearch(sortedArray, 14, 0, sortedArray.length - 1));
     }
 }
